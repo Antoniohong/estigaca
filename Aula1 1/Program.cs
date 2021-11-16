86d9296e0ca98e3cf70b0f675c33526d4732e5fb
Antoniohong <94448302+Antoniohong@users.noreply.github.com>
2021-11-16 10:50:28 +0000

    Add files via upload

---
+++ Aula1 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double dinheiro = 0.10;
+			string nome = "António";
+			int filhos = 12;
+
+			Console.WriteLine("Olá meu nome é " + nome + " tenho uma familia de " + filhos + " filhos, e para puder sustentá-los eu ganho " + dinheiro + " centimos por ano");
+
+			Console.ReadLine();
+		}
+	}
+}
